5ddd3e949956d9a5c977a5de68b24bc17d597182
marcpre <marcus.presich@gmail.com>
2018-03-14 05:54:40 +0000

    configure babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,33 @@
-/* 
-    ./webpack.config.js
-*/
 const path = require('path');
-
+/*
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
     template: './client/index.html',
     filename: 'index.html',
     inject: 'body'
 })
-
+*/
 module.exports = {
-    entry: './client/index.js',
+    devtool: 'cheap-module-eval-source-map'
+    entry: './src/index.js',
     output: {
-        path: path.resolve('dist'),
-        filename: 'index_bundle.js'
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: ''
+    },
+    resolve: {
+      extensions: ['.js', '.jsx']  
     },
     module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ],
+
+/*
         loaders: [{
                 test: /\.js$/,
                 loader: 'babel-loader',
@@ -28,6 +39,6 @@ module.exports = {
                 exclude: /node_modules/
             }
         ]
-    },
+*/    },
     plugins: [HtmlWebpackPluginConfig]
 }
